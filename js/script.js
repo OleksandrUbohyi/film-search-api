7446c860f292ebece9ca63ed55c8f51a533d96e6
ZadorozhnyiOleksandr <global2@mail.ua>
2019-07-29 17:24:43 +0300

    created film search api project

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict"
+const form = document.querySelector('#search-form');
+
+
+
+function apiSearch(e) {
+    e.preventDefault();
+    let searchText = document.querySelector('#search-text').value;
+    let server = 'https://api.themoviedb.org/3/search/multi?api_key=1983cecd00be37d37b4510809b6e5780&language=ru&query=' + searchText;
+
+    requestAPI('GET', server);
+}
+
+form.addEventListener('submit', apiSearch);
+
+function requestAPI(method, url) {
+    let request = new XMLHttpRequest();
+    let movie = document.querySelector('#movies');
+
+    request.open(method, url);
+    request.send();
+
+    request.addEventListener('readystatechange', function() {
+        if(request.readyState !== 4) return;
+        if(request.status!==200) { //если запрос не успешный
+            console.log('error ' + request.status);
+        }
+
+        const output = JSON.parse(request.responseText); // правильно форматируем длинную JSON строку
+        console.log(output);
+        let inner = "";
+      
+        output.results.forEach(function(item) {
+            let itemName = item.name || item.title;
+            let itemPoster = item.poster_path;
+            let releaseDate = item.release_date || item.first_air_date;
+            let img = 'https://image.tmdb.org/t/p/original/' + itemPoster;
+            console.log(img);
+            inner +=`<div class="col-12 col-sm-6 col-lg-4 col-xl-3  mb-2">
+                        <div class="border border-warning h-100">
+                            <img src="${img}">
+                            <div><b>Название фильма:</b> ${itemName}</div>
+                                <div><b>Дата выхода первой серии:</b> <span class="badge badge-warning">${releaseDate}</span></div>
+                            </div>
+                    </div>`;
+            movie.innerHTML = inner;
+        });
+    })
+}
